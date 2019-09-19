e4c181930443911ff40748646fcd404de44cc2ae
juan_lerch <juanlerch@gmail.com>
2019-09-19 17:46:42 -0300

    nivel01
    nivel01

---
+++ Assets/Scripts/Camera2Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2Player : MonoBehaviour
+{
+    
+    public GameObject player;       //Public variable to store a reference to the player game object
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    
+       void Start()
+    {
+        if (!player) player = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position - player.transform.position;
+    }
+
+     void LateUpdate () 
+    {
+      float y = transform.position.y;
+        Vector3  position = player.transform.position + offset;
+        position.y=y;
+        transform.position= position;
+    }
+}
---
+++ Assets/Scripts/Loser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using  UnityEngine.SceneManagement; 
+using UnityEngine;
+
+public class Loser : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (transform.position.y<-5)         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
