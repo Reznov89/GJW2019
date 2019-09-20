71368d8afe188f749c52eed8ac73dd5e047a860a
matiasff <matybienalto@gmail.com>
2019-09-20 18:43:45 -0300

    fin de jam
    Se sube los cambios al momento de finalizar la jam

---
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    public GameObject texto;
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D otro)
+    {
+        if (otro.gameObject.CompareTag("Finish"))
+        {
+            texto.SetActive(true);
+        }
+    }
+}
